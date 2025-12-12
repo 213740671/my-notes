e855987f203352a72cd6015dbf8107dd4a8b480f
ZhongYifan <213740671@qq.com>
2025-12-12 16:59:12 +0800

    vault backup: 2025-12-12 16:59:12

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "zyf's obsidian notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
